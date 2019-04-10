07e4fc433d34b5e49628d9e6f74ac75c44b20f8f
ety001 <ety001@domyself.me>
2019-04-10 22:37:45 +0800

    database structure

---
+++ database/migrations/2019_04_10_022221_create_app_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app', function (Blueprint $table) {
+            $table->string('app_id', 16);
+            $table->string('app_name', 100)->nullable();
+            $table->string('app_icon', 200)->nullable();
+            $table->text('app_description')->nullable();
+            $table->string('website', 100)->nullable();
+            $table->string('callback_uri', 200)->nullable();
+            $table->string('test_callback_uri', 200)->nullable();
+            $table->string('main_key');
+            $table->string('posting_key');
+            $table->string('active_key');
+            $table->string('memo_key');
+            $table->smallInteger('status')->default(1);
+            $table->timestamps();
+            $table->primary('app_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app');
+    }
+}
--- database/migrations/2019_04_10_023108_create_auth_table.php
+++ database/migrations/2019_04_10_023108_create_auth_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAuthTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('auth', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('user_id', 16)->index();
+            $table->string('app_id', 16)->index();
+            $table->string('scopes');
+            $table->string('token', 64);
+            $table->timestamp('expire_at')->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('auth');
     }
 }
