ce81d78915cde6b5a0df27e2845c8c24bc97fffd
ETY001 <ety001@domyself.me>
2019-06-23 21:22:49 +0800

    fix style of index page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,12 +6,36 @@
     <div class="row">
         <div class="col-xs-12">
             <h1>AuthSteem</h1>
-            @if (env('APP_ENV') == 'local')
-            <p><a href="{{ route('auth', ['app_id' => 'authsteem', 'scope' => 'login', 'test' => 1]) }}">开始</a></p>
-            @else
-            <p><a href="{{ route('auth', ['app_id' => 'authsteem', 'scope' => 'login']) }}">开始</a></p>
-            @endif
-            <p><a href="{{ route('home_docs') }}">文档</a></p>
+            <ul class="menu">
+                @if (env('APP_ENV') == 'local')
+                <li><a href="{{ route('auth', ['app_id' => 'authsteem', 'scope' => 'login', 'test' => 1]) }}">登录</a></li>
+                @else
+                <li><a href="{{ route('auth', ['app_id' => 'authsteem', 'scope' => 'login']) }}">登录</a></li>
+                @endif
+                @if (env('APP_ENV') == 'local')
+                <li><a href="{{ route('auth', ['app_id' => 'authsteem', 'scope' => 'posting', 'test' => 1]) }}">注册</a></li>
+                @else
+                <li><a href="{{ route('auth', ['app_id' => 'authsteem', 'scope' => 'posting']) }}">注册</a></li>
+                @endif
+                <li><a href="{{ route('home_docs') }}">文档</a></li>
+            </ul>
         </div>
     </div>
+@endsection
+
+@section('customcss')
+<style>
+.menu { 
+    font: 18px verdana, arial, sans-serif;
+}
+.menu, .menu li {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+.menu li {
+    float: left;
+    margin-right: 30px;
+}
+</style>
 @endsection
