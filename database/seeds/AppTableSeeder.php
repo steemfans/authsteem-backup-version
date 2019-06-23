c9e8486e63210bc85583bf7332a10bdefc04e8d4
ETY001 <ety001@domyself.me>
2019-06-23 18:00:24 +0800

    update app info

--- database/seeds/AppTableSeeder.php
+++ database/seeds/AppTableSeeder.php
@@ -19,7 +19,7 @@ class AppTableSeeder extends Seeder
         $app->app_desc = '面向国内的Steem登录验证平台';
         $app->app_site = 'https://connect.steemtools.top';
         $app->cb_uri = 'https://connect.steemtools.top/callback';
-        $app->test_cb_uri = '';
+        $app->test_cb_uri = 'http://laravel.test/callback';
         $app->username = 'authsteem';
         $app->secret = md5(env('APP_KEY'));
         $app->save();
