253eeff510099c02d25f35ec808c83d863910e77
ETY001 <ety001@domyself.me>
2019-06-23 19:55:57 +0800

    update logo

--- database/seeds/AppTableSeeder.php
+++ database/seeds/AppTableSeeder.php
@@ -15,7 +15,7 @@ class AppTableSeeder extends Seeder
     {
         $app = new App();
         $app->app_name = 'AuthSteem';
-        $app->app_icon = 'https://i.loli.net/2019/06/22/5d0de7b0dd32f71915.png';
+        $app->app_icon = 'https://connect.steemtools.top/img/logo.png';
         $app->app_desc = '面向国内的Steem登录验证平台';
         $app->app_site = 'https://connect.steemtools.top';
         $app->cb_uri = 'https://connect.steemtools.top/callback';
